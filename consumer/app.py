ca1807033808c930ed358eb18cec1172de95ab51
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-05-17 14:39:28 +0300

    [UPD]: add celery

--- consumer/app.py
+++ consumer/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from celery import Celery
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], include=['tasks'])
+celery.conf.update(app.config)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
