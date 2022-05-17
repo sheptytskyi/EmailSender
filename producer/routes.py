acaf95135588d632ff6d2352e2770a2b4ec257fd
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-05-17 14:30:32 +0300

    [UPD]: add post method to routes

--- producer/routes.py
+++ producer/routes.py
@@ -13,3 +13,14 @@ db = SessionLocal()
 @router.get('/')
 def get_all_emails() -> List[Emails]:
     return db.query(Emails).all()
+
+
+@router.post('/send')
+def send(item: EmailSchema) -> EmailSchema:
+    new_mail = Emails(email=item.email, message=item.message)
+
+    db.add(new_mail)
+    db.commit()
+    db.refresh(new_mail)
+
+    return new_mail
