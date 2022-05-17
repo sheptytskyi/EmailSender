c680ea2352e05e0ab71fa028e23f7942a8729be6
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-05-17 14:27:44 +0300

    [ADD]: routing

--- producer/app.py
+++ producer/app.py
@@ -4,10 +4,12 @@ from fastapi import FastAPI
 
 from producer.db import engine
 from producer import models
+from producer.routes import router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(router)
 
 
 if __name__ == '__main__':
